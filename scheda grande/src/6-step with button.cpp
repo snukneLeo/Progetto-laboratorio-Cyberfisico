8bc58820afca6b98af5e637b43919d6b00378e39
Snukne <leonardo.testolin@studenti.univr.it>
2018-09-03 15:37:59 +0200

    upload files

---
+++ scheda grande/src/6-step with button.cpp
@@ -0,0 +1,54 @@
+    //6-step with button
+   /* stepForeward(step_number % 6);
+    while(1)
+    {
+        //pwm_positive = input.read(); 
+        if (mybutton == 0){
+            stepForeward(step_number % 6);
+        
+        //if (count > 100){
+            //count = 0;
+            myled = !myled;
+            hallA = readA.read();
+            hallB = readB.read();
+            hallC = readC.read();
+            pc.printf("%d,%d,%d\n",hallA, hallB, hallC);
+            pc.printf("%d\n", step_number % 6);
+            step_number++;
+            wait(0.2f);
+            //pc.printf("%d\n", step_number % 6);
+            
+        }
+    }
+
+        /*hallA = readA.read();
+        hallB = readB.read();
+        hallC = readC.read();
+        pc.printf("%d,%d,%d\n",hallA, hallB, hallC);
+        pc.printf("%d\n", step_number % 6);
+    }*/
+        //}
+        
+        //ount++;
+
+        // read the angle
+
+        //angle_deg = encoder.getAngleDeg();
+        //angle_rad = encoder.getAngleRad();
+        /*hallA = readA.read();
+        hallB = readB.read();
+        hallC = readC.read();
+
+
+        //stepForeward(hallStepRead(hallA,hallB,hallC));
+        //wait(0.5f);
+        pc.printf("%d,%d,%d\n",hallA, hallB, hallC);*/
+        //pc.printf("%i\n",readA.read());
+
+        // every 7 seconds reset the angle
+        //count++;
+        //if(count>70) {
+        //    count = 0;
+        //    encoder.reset();
+        //}
+        //wait(0.001f);*/
--- scheda grande/src/main.cpp
+++ scheda grande/src/main.cpp
@@ -59,15 +59,6 @@ void set100hz() //100hz
     mTimUserHandle.Init.ClockDivision   = TIM_CLOCKDIVISION_DIV1;
 }
 
-/*void set100hz() //100hz
-{
-    mTimUserHandle.Instance             = TIM_USR;
-    mTimUserHandle.Init.Prescaler       = 10000;
-    mTimUserHandle.Init.CounterMode     = TIM_COUNTERMODE_UP;
-    mTimUserHandle.Init.Period          = 83 + 1;
-    mTimUserHandle.Init.ClockDivision   = TIM_CLOCKDIVISION_DIV1;
-}*/
-
 // 1 Hz, 100Hz, 1KHz, 10KHz
 // CLOCK_FREQUENCY = 84000000.0f
 // TIM_FREQUENCY = (int) (CLOCK_FREQUENCY / mTimUserHandle.Init.Prescaler + 1 / mTimUserHandle.Init.Period + 1)
@@ -99,7 +90,7 @@ DigitalEncoderAB encoder(4096);
 // PWM duty cycle for motor phase
 
 float pwm_positive = 0.2f;//0.5f;
-float gnd_negative = -0.2f;
+float gnd_negative = -pwm_positive;
 
 // ON board LED
 //DigitalOut led1(LED1);
@@ -120,89 +111,34 @@ DigitalOut en_2(PB_0);
 DigitalOut en_3(PB_1);
 // MOTOR DRIVER CHIP ENABLE PIN
 DigitalOut en_chip(PA_6); //PA_6
-/*
-// This function converts hall sensor's angle into 6 electrical position of 
-void stepRead()
-{
-    // Check in which of 6 position the motor is
-    if(position<=8.97f || position>50.5f)
-    {
-        step_number=5;
-    }
-    if (position <= 8.35f || position <= 17.49f)
-    {
-        step_number = 4;
-    }
-    if (position <= 8.97f || position > 50.5f)
-    {
-        step_number = 3;
-    }
-    if (position <= 8.97f || position > 50.5f)
-    {
-        step_number = 2;
-    }
-    if (position <= 8.97f || position > 50.5f)
-    {
-        step_number = 1;
-    }
-    if (position <= 8.97f || position > 50.5f)
-    {
-        step_number = 0;
-    }
-}
-*/
 
 // This function converts hall sensor's read into 6 electrical position of 
 int hallStepRead(int hallA, int hallB, int hallC)
 {
     // Check in which of 6 read of hall sensor
-    /*if(hallA == false && hallB == true && hallC == false)
-    {
-        return 5;//step_number=1;
-    }
-    if (hallA == true && hallB == true && hallC == false)
-    {
-        return 4;//step_number = 2;
-    }
-    if (hallA == true && hallB == false && hallC == false)
-    {
-        return 3;//step_number = 3;
-    }
-    if (hallA == true && hallB == false && hallC == true)
-    {
-        return 2;//step_number = 4;
-    }
-    if (hallA == false && hallB == false && hallC == true)
-    {
-        return 1;//step_number = 5;
-    }
-    if (hallA == false && hallB == true && hallC == true)
-    {
-        return 0;//step_number = 6;
-    }*/
-    if(hallA == 0 && hallB == 1 && hallC == 0)
+    if(hallA == 0 && hallB == 1 && hallC == 0) //3
     {
         return 3;//step_number=1;
     }
-    if (hallA == 1 && hallB == 1 && hallC == 0)
+    if (hallA == 1 && hallB == 1 && hallC == 0) //2
     {
         return 2;//step_number = 2;
     }
-    if (hallA == 1 && hallB == 0 && hallC == 0)
+    if (hallA == 1 && hallB == 0 && hallC == 0) //1
     {
         return 1;//step_number = 3;
     }
-    if (hallA == 1 && hallB == 0 && hallC == 1)
+    if (hallA == 1 && hallB == 0 && hallC == 1) //0
     {
         return 0;//step_number = 4;
     }
-    if (hallA == 0 && hallB == 0 && hallC == 1)
+    if (hallA == 0 && hallB == 0 && hallC == 1) //5
     {
         return 5;//step_number = 5;
     }
-    if (hallA == 0 && hallB == 1 && hallC == 1)
+    if (hallA == 0 && hallB == 1 && hallC == 1) //4
     {
-        return 0;//step_number = 6;
+        return 4;//step_number = 6;
     }
 }
 
@@ -270,29 +206,23 @@ void stepForeward(int step_number)
 
 int main()
 {
-    uh_1.period(0.00001f);
-    vh_2.period(0.00001f);
-    wh_3.period(0.00001f);
+    uh_1.period(0.0001f);
+    vh_2.period(0.0001f);
+    wh_3.period(0.0001f);
     // for printing float values
     asm(".global _printf_float");
 
-    //float angle_deg, angle_rad;
-    /*bool*/ int hallA, hallB, hallC;
+    int hallA, hallB, hallC;
 
-    //encoder.setAngleFormat(angleMod);
-
-    int count = 0;
     int step_number = 0;
     en_chip = 1;
-    //stepForeward(step_number % 6);
 
     uint16_t saples[1024]; 
 
     // Enable timer
     __HAL_RCC_TIM3_CLK_ENABLE();
 
-    set100hz();
-
+    set10khz(); //set timer
 
     HAL_TIM_Base_Init(&mTimUserHandle);
     HAL_TIM_Base_Start_IT(&mTimUserHandle);
@@ -301,90 +231,22 @@ int main()
     NVIC_EnableIRQ(TIM_USR_IRQ);
 
     tim.start();
-
-    //stepForeward(step_number % 6);
+    
     while(1) 
     {
         
         if (flag_time == 1)
         {
-            //pwm_positive = input.read(); 
+            pwm_positive = input.read(); 
             hallA = readA.read();
             hallB = readB.read();
             hallC = readC.read();
 
             step_number = hallStepRead(hallA,hallB,hallC);
             stepForeward(step_number);
-            pc.printf("%d,%d,%d\n",hallA, hallB, hallC);
-            //pc.printf("%d\n",step_number);
+            //pc.printf("%d,%d,%d\n",hallA, hallB, hallC);
+            pc.printf("%d\n",step_number);
             flag_time = 0;
         }
-        
-        /*uh_1.write(pwm_positive);       //PWM
-        vh_2.write(0.0f);               //DISCONNECTED
-        wh_3.write(gnd_negative);       //GND
-        en_1 = 1;                       //PWM
-        en_2 = 0;                       //DISCONNECTED
-        en_3 = 1;   
-        hallA = readA.read();
-        hallB = readB.read();
-        hallC = readC.read();
-
-    
-        pc.printf("%d,%d,%d\n",hallA, hallB, hallC);*/
     }
-    
-   /* stepForeward(step_number % 6);
-    while(1)
-    {
-        //pwm_positive = input.read(); 
-        if (mybutton == 0){
-            stepForeward(step_number % 6);
-        
-        //if (count > 100){
-            //count = 0;
-            myled = !myled;
-            hallA = readA.read();
-            hallB = readB.read();
-            hallC = readC.read();
-            pc.printf("%d,%d,%d\n",hallA, hallB, hallC);
-            pc.printf("%d\n", step_number % 6);
-            step_number++;
-            wait(0.2f);
-            //pc.printf("%d\n", step_number % 6);
-            
-        }
-    }
-
-        /*hallA = readA.read();
-        hallB = readB.read();
-        hallC = readC.read();
-        pc.printf("%d,%d,%d\n",hallA, hallB, hallC);
-        pc.printf("%d\n", step_number % 6);
-    }*/
-        //}
-        
-        //ount++;
-
-        // read the angle
-
-        //angle_deg = encoder.getAngleDeg();
-        //angle_rad = encoder.getAngleRad();
-        /*hallA = readA.read();
-        hallB = readB.read();
-        hallC = readC.read();
-
-
-        //stepForeward(hallStepRead(hallA,hallB,hallC));
-        //wait(0.5f);
-        pc.printf("%d,%d,%d\n",hallA, hallB, hallC);*/
-        //pc.printf("%i\n",readA.read());
-
-        // every 7 seconds reset the angle
-        //count++;
-        //if(count>70) {
-        //    count = 0;
-        //    encoder.reset();
-        //}
-        //wait(0.001f);*/
 }
